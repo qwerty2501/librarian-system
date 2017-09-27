8289d4ceeae58eaf31d70525a409219090a10ed7
qwerty2501 <qwerty2501@live.jp>
2017-09-28 02:36:09 +0900

    modified members for JWT.

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -1,7 +1,8 @@
 package models
 
 import java.sql.Timestamp
+import java.time.LocalDateTime
 
 case class User(id:Int, mail:String, password:String, name:String, createdAt:Timestamp,updatedAt:Timestamp)
 
-case class UserRegistrationRequest(requestKey:String,mail:String,expiredAt:Timestamp, createdAt:Timestamp,updatedAt:Timestamp)
+case class UserRegistrationRequest(mail:String,expiredAt:LocalDateTime)
