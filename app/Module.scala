1ad767adb35b6560597f0fe3ea759acaef321e58
qwerty2501 <qwerty2501@live.jp>
2017-09-28 02:35:44 +0900

    deleted injections.

--- app/Module.scala
+++ app/Module.scala
@@ -8,12 +8,6 @@ import services._
 class Module extends AbstractModule {
 
   override def configure() = {
-
-    bind(classOf[UserService]).to(classOf[UserServiceImpl])
-
-    bind(classOf[UserDAO]).to(classOf[UserDAOImpl])
-
-    bind(classOf[UserRegistrationRequestDAO]).to(classOf[UserRegistrationRequestDAOImpl])
   }
 
 }
