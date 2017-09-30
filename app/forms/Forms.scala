7f6400300708e29b1242679c5bb4c2c94c9ced6e
qwerty2501 <qwerty2501@live.jp>
2017-09-30 21:42:56 +0900

    commonify validations.

--- app/forms/Forms.scala
+++ app/forms/Forms.scala
@@ -4,3 +4,5 @@ package forms
 case class StartCreateUserRequestForm(mail:String)
 
 case class CreateUserForm(name:String,password:String,passwordConfirm:String)
+
+case class LoginForm(mail:String,password:String)
